dacfde989369a9ff8c69d24f26e9d243707520d8
sriramreddydwarampudi <130766784+sriramreddydwarampudi@users.noreply.github.com>
2025-07-14 09:46:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+
 #!/usr/bin/env python3
 """
 Enhanced Music21 Visual DAW - Scale Interval Display with Horizontal Scroll
